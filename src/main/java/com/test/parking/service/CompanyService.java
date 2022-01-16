ff7cf51539d0c6158d4d097e54fc578726143f07
Ítalo Pinto <italosp00@gmail.com>
2022-01-16 20:16:03 -0300

    Update company service layer
    - Added routines to deal with the parking spaces
    - How we need the parking spaces to be managed by the company
    - I thought tha this was the right to do, at least by now

--- src/main/java/com/test/parking/service/CompanyService.java
+++ src/main/java/com/test/parking/service/CompanyService.java
@@ -2,18 +2,48 @@ package com.test.parking.service;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import com.test.parking.model.Company;
+import com.test.parking.model.ParkingSpace;
 import com.test.parking.repository.CompanyRepository;
+import com.test.parking.repository.ParkingSpaceRepository;
 
 @Service
 public class CompanyService {
-	
+	/*
+	 * Criar as vagas de estacionamento na tabela de estacionamento
+	 * no momento da criação da company ✔
+	 * 
+	 * como vai ser quando atualizar o numero de vagas de estacionamento por tipo? ✔
+	 * 
+	 * O controller não deve deixar vir valores negativos de número de vagas de estacionamento
+	 * */
 	@Autowired
 	private CompanyRepository companyRepository;
 	
+	@Autowired
+	private ParkingSpaceRepository parkingSpace;
+	
 	public Company addCompany(Company company) {
+		int motorcycle = company.getMotorcyclesSpace();
+		int car = company.getCarsSpace();
+		ParkingSpace newSpace = new ParkingSpace();
+		if (motorcycle != 0) {
+			for (int i = 1; i <= motorcycle; i++) {
+				newSpace.setSpaceNumber(i);
+				newSpace.setSpaceType("motorcycle");
+				parkingSpace.save(newSpace);
+			}
+		}
+		if (car != 0) {
+			for (int i = 1; i <= car; i++) {
+				newSpace.setSpaceNumber(i);
+				newSpace.setSpaceType("car");
+				parkingSpace.save(newSpace);
+			}
+		}
 		return companyRepository.save(company);
 	}
 	
@@ -38,6 +68,33 @@ public class CompanyService {
 		existingCompany.setPhone(company.getPhone());
 		existingCompany.setMotorcyclesSpace(company.getMotorcyclesSpace());
 		existingCompany.setCarsSpace(company.getCarsSpace());
+		
+		ParkingSpace newSpace = new ParkingSpace();
+		
+		int carsDifferenceSpaces = existingCompany.getCarsSpace() - company.getCarsSpace();
+		if (carsDifferenceSpaces < 0) {
+			for (int i = 1; i <= carsDifferenceSpaces*-1; i++) {
+				newSpace.setSpaceNumber(i);
+				newSpace.setSpaceType("car");
+				parkingSpace.save(newSpace);
+			}
+		} else if (carsDifferenceSpaces > 0) {
+			List<ParkingSpace> spacesToDelete = parkingSpace.findAll(Sort.by("car").and(Sort.by("space_number").descending()));
+			parkingSpace.deleteAll(spacesToDelete);
+		}
+		
+		int motorcyclesDifferenceSpaces = existingCompany.getMotorcyclesSpace() - company.getMotorcyclesSpace();
+		if (motorcyclesDifferenceSpaces < 0) {
+			for (int i = 1; i <= motorcyclesDifferenceSpaces*-1; i++) {
+				newSpace.setSpaceNumber(i);
+				newSpace.setSpaceType("motorcycle");
+				parkingSpace.save(newSpace);
+			}
+		} else if (motorcyclesDifferenceSpaces > 0) {
+			List<ParkingSpace> spacesToDelete = parkingSpace.findAll(Sort.by("motorcycle").and(Sort.by("space_number").descending()));
+			parkingSpace.deleteAll(spacesToDelete);
+		}
+		
 		return companyRepository.save(existingCompany);
 	}
 }
