948d743c8f70e3290c1a31223b7d021c283dbb19
Ítalo Pinto <italosp00@gmail.com>
2022-01-18 17:19:24 -0300

    Fix the code adding @Repository annotation

--- src/main/java/com/test/parking/repository/VehicleRepository.java
+++ src/main/java/com/test/parking/repository/VehicleRepository.java
@@ -1,9 +1,11 @@
 package com.test.parking.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.test.parking.model.Vehicle;
 
+@Repository
 public interface VehicleRepository extends JpaRepository<Vehicle, Long>{
 
 }
