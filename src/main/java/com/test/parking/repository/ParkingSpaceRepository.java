61fde408da173211b2c0994e770fd6289e1cbbf4
Ítalo Pinto <italosp00@gmail.com>
2022-01-18 17:20:52 -0300

    Fix and create
    - Fix the missing annotation @Repository
    - Create a method to make a custon Query in the database with annotation @Query

--- src/main/java/com/test/parking/repository/ParkingSpaceRepository.java
+++ src/main/java/com/test/parking/repository/ParkingSpaceRepository.java
@@ -1,9 +1,16 @@
 package com.test.parking.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import com.test.parking.model.ParkingSpace;
 
+@Repository
 public interface ParkingSpaceRepository extends JpaRepository<ParkingSpace, Long> {
 
+	@Query(value = "SELECT * FROM parking_space WHERE space_type = ?1 ORDER BY space_number DESC", nativeQuery = true)
+	List<ParkingSpace> findAllSpaces(String spaceType);
 }
